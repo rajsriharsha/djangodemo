43d701fe2dca33ee5f313362a286223504e7bfdb
Harsha <rajsriharsha@gmail.com>
2021-07-13 22:45:17 +1000

    13_July_2021 - Feature2

--- sortnpage/models.py
+++ sortnpage/models.py
@@ -17,7 +17,7 @@ class Incident(models.Model):
     date = models.DateField()
     incident_notes = models.TextField(default="")
     classification = models.CharField(
-        max_length=10,verbose_name='Security Classifications'
+        max_length=10,verbose_name='Security Classifications_added'
     )
 
     def save(self, *args, **kwargs):
