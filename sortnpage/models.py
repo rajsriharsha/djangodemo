30f8e96ab7e242450be6c0a8cb17ea40fe4ce1d9
rajsriharsha <80179733+rajsriharsha@users.noreply.github.com>
2021-07-13 23:04:35 +1000

    Update models.py

--- sortnpage/models.py
+++ sortnpage/models.py
@@ -17,7 +17,7 @@ class Incident(models.Model):
     date = models.DateField()
     incident_notes = models.TextField(default="")
     classification = models.CharField(
-        max_length=10,verbose_name='Security Classifications_added_13july3_OnlineEdited'
+        max_length=10,verbose_name='Security Classifications_added_13july3_OnlineEdited2'
     )
 
     def save(self, *args, **kwargs):
