48ef28107c1c705c2c8faa0384026e46caed79e0
Harsha <rajsriharsha@gmail.com>
2022-07-30 09:28:19 +1000

    30 July - COmmit1

--- bulkupdate/admin.py
+++ bulkupdate/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Todo, Todo_Child
+from .models import Todo, Todo_Child, Type, Status, Region
 
 # Register your models here.
 # class ParentAdmin(admin.ModelAdmin):
@@ -10,3 +10,6 @@ from .models import Todo, Todo_Child
 
 admin.site.register(Todo)
 admin.site.register(Todo_Child)
+admin.site.register(Status)
+admin.site.register(Region)
+
--- bulkupdate/models.py
+++ bulkupdate/models.py
@@ -23,3 +23,55 @@ class Todo(models.Model):
 class Todo_Child(models.Model):
     sub_task = models.CharField(max_length=100)
     active_flag = models.BooleanField()
+
+class Type(models.Model):
+
+    type_id = models.AutoField(
+        primary_key = True,verbose_name = 'Type ID'
+    )
+    type_name = models.CharField(
+        max_length=100, verbose_name='Type Name'
+    )
+    type_desc = models.TextField(
+        null=True, blank=True
+    )
+    active_status = models.BooleanField(
+        default=True, verbose_name='Active ? '
+    )
+    remarks = models.TextField(
+        default='',verbose_name='Remarks'
+    )
+
+    class Meta:
+        verbose_name = 'Type'
+        verbose_name_plural = 'Type'
+        ordering = ['type_name',]
+        unique_together = ['type_name','active_status']
+        db_table = 'authorisations_type'
+
+    def __str__(self):
+        return self.type_name + str(self.type_id)
+
+
+class Status(models.Model):
+    status_id = models.AutoField(
+        primary_key=True,
+    )
+    status_name = models.CharField(
+        max_length=100,
+    )
+
+    def __str__(self):
+        return self.status_name
+
+
+class Region(models.Model):
+    region_id = models.AutoField(
+        primary_key=True,
+    )
+    region_name = models.CharField(
+        max_length=100,
+    )
+
+    def __str__(self):
+        return self.region_name
