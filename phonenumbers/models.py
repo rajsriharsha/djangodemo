9f28b9ebc25b9ebcb0406aed7434d96178cd79a7
rajsriharsha <80179733+rajsriharsha@users.noreply.github.com>
2022-07-30 13:39:55 +1000

    New Countrycode table @ 1:30 pm
    New Countrycode table @ 1:30 pm

--- phonenumbers/models.py
+++ phonenumbers/models.py
@@ -17,3 +17,14 @@ class CountryCode(models.Model):
     )
     geography_remarks = models.TextField(max_length=1000)
 
+    
+    class CountryCode_New(models.Model):
+    name = models.CharField(max_length=127)
+    country_code = models.CharField(max_length=40)
+    country_info = models.TextField(
+        max_length=1000,
+        null=True,
+        blank=True,
+        default=""
+    )
+    geography_remarks = models.TextField(max_length=1000)
