aac0ff0d9b263813fbd1e9e48fc9de0f7c5d8a81
Harsha <rajsriharsha@gmail.com>
2022-07-30 11:44:41 +1000

    Added Remarks in phone numbers - countrycode table

--- phonenumbers/models.py
+++ phonenumbers/models.py
@@ -9,3 +9,8 @@ class PhoneNumbers(models.Model):
 class CountryCode(models.Model):
     name = models.CharField(max_length=127)
     country_code = models.CharField(max_length=5)
+    remarks = models.TextField(
+        default="",
+        null=True,
+        blank=True
+    )
