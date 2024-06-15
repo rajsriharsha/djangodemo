d8b5bda27f122a3a79f04b4a56121338eb18816e
rajsriharsha <80179733+rajsriharsha@users.noreply.github.com>
2024-06-15 21:24:06 +1000

    vIN - Added admin.py for Location and Country

--- countrydropdown/admin.py
+++ countrydropdown/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from countrydropdown.models import *
+# Register your models here.
+# class ParentAdmin(admin.ModelAdmin):
+#     model = Parent
+
+# Start - Changes made by Harsha- Integration Team
+admin.site.register(Location)
+admin.site.register(CountrySelect)
+# End - Changes made by Harsha - Integration Team
 
 # Register your models here.
