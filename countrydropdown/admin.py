4e58f726a07b4fa2cebc325c0629a3e70ab02d85
rajsriharsha <80179733+rajsriharsha@users.noreply.github.com>
2024-06-15 20:58:58 +1000

    vIN - Changes by Integration Team

--- countrydropdown/admin.py
+++ countrydropdown/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from countrydropdown.models import *
+# Register your models here.
+# class ParentAdmin(admin.ModelAdmin):
+#     model = Parent
+
+# Start - Changes made by Integration Team
+admin.site.register(Location)
+admin.site.register(CountrySelect)
+# End - Changes made by Integration Team
 
 # Register your models here.
